38f2497c8afbcdd440e1b6ae785e44ec25751097
AhmedSami36 <ahmedsami1423@gmail.com>
2025-01-13 02:52:28 +0200

    removed the employee column

--- src/main/java/com/example/gymmanagementsystem/Gym/gymEntity.java
+++ src/main/java/com/example/gymmanagementsystem/Gym/gymEntity.java
@@ -10,15 +10,13 @@ public class gymEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long gym_id;
 
-    @Column(nullable = false,unique=true)
+    @Column(nullable = false, unique = true)
     private String gym_name;
 
-    @Column(nullable = false,unique=true)
+    @Column(nullable = false, unique = true)
     private String gym_location;
-    @Column(nullable = true,unique=true)
+    @Column(nullable = true, unique = true)
     private String gym_description;
-//    @Column
-//    @OneToMany(mappedBy = gymEntity, cascade = CascadeType.ALL)
-//    private List<employee> gym_employees;
-    
+
 }
+
