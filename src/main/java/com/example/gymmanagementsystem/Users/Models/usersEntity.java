008e1736ce549e87d79c3e1367165ab2eeec9e29
AhmedSami36 <ahmedsami1423@gmail.com>
2025-02-02 00:42:01 +0200

    adding user columns to the entity

--- src/main/java/com/example/gymmanagementsystem/Users/Models/usersEntity.java
+++ src/main/java/com/example/gymmanagementsystem/Users/Models/usersEntity.java
@@ -3,6 +3,8 @@ package com.example.gymmanagementsystem.Users.Models;
 import com.example.gymmanagementsystem.Gym.Models.gymEntity;
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity(name = "Users")
 public class usersEntity {
     @Id
@@ -12,7 +14,17 @@ public class usersEntity {
     @Column(nullable = false)
     private String usr_name;
 
+    private Date birthDate;
+    private String email;
+    private String password;
+    private String phone;
+    private String gender;
+    private String role;
+
+
     @ManyToOne
     @JoinColumn(name = "gym_id", nullable = false)
     private gymEntity gym;
+
+    
 }
