3ef2d4afb9db2ad8159216cd08b46bf2e1ff4eb0
AhmedSami36 <ahmedsami1423@gmail.com>
2025-02-02 12:37:16 +0200

    adjustment the gym controller

--- src/main/java/com/example/gymmanagementsystem/Gym/Controllers/gymController.java
+++ src/main/java/com/example/gymmanagementsystem/Gym/Controllers/gymController.java
@@ -1,7 +1,27 @@
 package com.example.gymmanagementsystem.Gym.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.gymmanagementsystem.Gym.Models.gymEntity;
+import com.example.gymmanagementsystem.Gym.Models.gymRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/gyms")
 public class gymController {
+
+    @Autowired
+    private gymRepository gymRepo;
+
+    @PostMapping("/create-gym")
+    public gymEntity createGym(@RequestBody gymEntity gymEntity) {
+       return gymRepo.save(gymEntity);
+    }
+
+    @GetMapping("/{gym_id}")
+    public ResponseEntity<gymEntity> getGym(@PathVariable Long gym_id) {
+        return gymRepo.findById(gym_id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
