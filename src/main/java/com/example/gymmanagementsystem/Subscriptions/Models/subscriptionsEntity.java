2a29209be76254986e721e31ce8021debd2f1c1f
AhmedSami36 <ahmedsami1423@gmail.com>
2025-02-02 11:53:27 +0200

    adjustment the subscriptions entity

--- src/main/java/com/example/gymmanagementsystem/Subscriptions/Models/subscriptionsEntity.java
+++ src/main/java/com/example/gymmanagementsystem/Subscriptions/Models/subscriptionsEntity.java
@@ -1,11 +1,37 @@
 package com.example.gymmanagementsystem.Subscriptions.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.example.gymmanagementsystem.DietPlans.Models.dietPlanEntity;
+import com.example.gymmanagementsystem.TrainingSchedules.Models.trainingScheduleEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
 
 @Entity(name = "subscriptions")
 public class subscriptionsEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int subscription_id;
+
+    @OneToOne
+    @JoinColumn(name = "diet_plan")
+    private dietPlanEntity diet_plan;
+
+    @Getter
+    @Setter
+    @Column(nullable = false)
+    private int price;
+
+    @Getter
+    @Setter
+    @Column(nullable = false)
+    private Date duration;
+
+    @Getter
+    @Setter
+    @OneToOne
+    @JoinColumn(name = "trianing_schedule")
+    private trainingScheduleEntity training_schedule;
 }
